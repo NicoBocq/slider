a30c1534ca46f8e8cfec048d2cc78de44485fa74
NicoBocq <nicolas.fow@gmail.com>
2022-01-19 12:08:55 +0100

    Fix vh height

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import Carousel from './Slider'
 import items from './items'
 import './styles.scss'
 
+// handle height in vh in css
+const appHeight = () => {
+  console.log('appHeight')
+  const vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+}
+window.addEventListener('load', appHeight)
+window.addEventListener('resize', appHeight)
+
 const App = () => (
   <Carousel items={items} />
 )
