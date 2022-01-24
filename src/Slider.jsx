a5baa6d99000fd1dc4df8fefbce93ab6b7333072
NicoBocq <nicolas.fow@gmail.com>
2022-01-24 14:26:50 +0100

    Evol label pinch

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -59,7 +59,7 @@ export default function Slider ({ items }) {
       //   return `<span class="${className}"></span>`
       // } else {
       return `<div class="${className}" style="background-image:url(${items[index].url_thumb})">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 9.2"><defs><style>.cls-1{fill:#188acb;}</style></defs><g id="Calque_2" data-name="Calque 2"><g id="Calque_1-2" data-name="Calque 1"><path id="camera-video-avec-bouton-de-lecture" class="cls-1" d="M15.3.6a1.06,1.06,0,0,0-1.2.2l-2,2.1V1.6A1.58,1.58,0,0,0,10.5,0H1.6A1.64,1.64,0,0,0,0,1.6v6A1.58,1.58,0,0,0,1.6,9.2h8.9a1.58,1.58,0,0,0,1.6-1.6V6.4l2,2.1a1.06,1.06,0,0,0,1.2.2,1.2,1.2,0,0,0,.7-1V1.6A1.2,1.2,0,0,0,15.3.6ZM7.6,5.1,5.4,6.8a.45.45,0,0,1-.5,0c-.2-.1-.3-.2-.3-.4V2.9a.55.55,0,0,1,.3-.5.47.47,0,0,1,.6.1L7.7,4.2a.77.77,0,0,1-.1.9c.1-.1,0-.1,0,0Z"/></g></g></svg>
+<!--        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 9.2"><defs><style>.cls-1{fill:#188acb;}</style></defs><g id="Calque_2" data-name="Calque 2"><g id="Calque_1-2" data-name="Calque 1"><path id="camera-video-avec-bouton-de-lecture" class="cls-1" d="M15.3.6a1.06,1.06,0,0,0-1.2.2l-2,2.1V1.6A1.58,1.58,0,0,0,10.5,0H1.6A1.64,1.64,0,0,0,0,1.6v6A1.58,1.58,0,0,0,1.6,9.2h8.9a1.58,1.58,0,0,0,1.6-1.6V6.4l2,2.1a1.06,1.06,0,0,0,1.2.2,1.2,1.2,0,0,0,.7-1V1.6A1.2,1.2,0,0,0,15.3.6ZM7.6,5.1,5.4,6.8a.45.45,0,0,1-.5,0c-.2-.1-.3-.2-.3-.4V2.9a.55.55,0,0,1,.3-.5.47.47,0,0,1,.6.1L7.7,4.2a.77.77,0,0,1-.1.9c.1-.1,0-.1,0,0Z"/></g></g></svg>-->
       </div>`
       // }
     }
@@ -77,6 +77,7 @@ export default function Slider ({ items }) {
                 </svg>
               </button>
               <div className="pinch-icon">
+                <span>Appuyez 2 fois ou pincez pour zoomer</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
